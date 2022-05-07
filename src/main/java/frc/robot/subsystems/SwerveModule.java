109e5414bbe70bde56efb7ceb1ebd1897bf0e2ae
Jason Kim <jason1234lol@gmail.com>
2022-05-06 23:01:36 -0500

    first commit

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,118 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.*;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.DriveConstants;
+import com.ctre.phoenix.sensors.CANCoder;
+import frc.robot.Constants.ModuleConstants;
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class SwerveModule {
+
+    private CANSparkMax drivingMotor;
+    private CANSparkMax turningMotor;
+
+    private RelativeEncoder drivingEncoder;
+    private RelativeEncoder turningEncoder;
+
+    private CANCoder absoluteEncoder;
+    private boolean absoluteEncoderReversed;
+    private double absoluteEncoderOffsetRad;
+    
+    private PIDController drivePIDController;
+    private PIDController turningPIDController;
+
+    
+
+    public SwerveModule(
+        int drivingMotorID, 
+        int turningMotorID, 
+        boolean drivingMotorReversed, 
+        boolean turningMotorReversed, 
+        int absoluteEncoderID, 
+        double absoluteEncoderOffset, 
+        boolean absoluteEncoderReversed) {
+            this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+            this.absoluteEncoderReversed = absoluteEncoderReversed;
+            absoluteEncoder = new CANCoder(absoluteEncoderID);
+    
+            drivingMotor = new CANSparkMax(drivingMotorID, MotorType.kBrushless);
+            turningMotor = new CANSparkMax(turningMotorID, MotorType.kBrushless);
+    
+            drivingMotor.setInverted(drivingMotorReversed);
+            turningMotor.setInverted(turningMotorReversed);
+    
+            drivingEncoder = drivingMotor.getEncoder();
+            turningEncoder = turningMotor.getEncoder();
+    
+            drivingEncoder.setPositionConversionFactor(ModuleConstants.kDriveEncoderRot2Meter);
+            drivingEncoder.setVelocityConversionFactor(ModuleConstants.kDriveEncoderRPM2MeterPerSec);
+            turningEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderRot2Rad);
+            turningEncoder.setVelocityConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec);
+    
+            turningPIDController = new PIDController(ModuleConstants.kTurnP, 0, 0);
+            turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+
+            resetEncoders();
+    }
+    
+    public double getDrivePosition() {
+        return drivingEncoder.getPosition();
+    }
+
+    public double getDriveVelocity() {
+        return drivingEncoder.getVelocity();
+    }
+
+    public double getTurningPosition() {
+        return turningEncoder.getPosition();
+    }
+
+    public double getTurningVelocity() {
+        return turningEncoder.getVelocity();
+    }
+
+    public double getAbsoluteEncoderRad() {
+        double angle = absoluteEncoder.getAbsolutePosition();
+        angle *= 2.0 * Math.PI;
+        angle -= absoluteEncoderOffsetRad;
+        return angle * (absoluteEncoderReversed ? -1.0 : 1.0);
+    }
+
+    public void resetEncoders() {
+        drivingEncoder.setPosition(0);
+        turningEncoder.setPosition(getAbsoluteEncoderRad());
+    }
+
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+    }
+
+    public void setDesiredState(SwerveModuleState state) {
+        if (Math.abs(state.speedMetersPerSecond) < 0.001) {
+            stop();
+            return;
+        }
+
+        state = SwerveModuleState.optimize(state, getState().angle);
+        drivingMotor.set(state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+        turningMotor.set(turningPIDController.calculate(getTurningPosition(), state.angle.getRadians()));
+        SmartDashboard.putString("Swerve[" + absoluteEncoder.getDeviceID() + "] state", state.toString());
+    }
+
+    public void stop() {
+        drivingMotor.set(0);
+        turningMotor.set(0);
+    }
+
+
+
+
+
+}
